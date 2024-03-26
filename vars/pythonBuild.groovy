4c7f805700c5333c819d0c9c91e1e5c593652753
Tristan Davis <Tristandavis888@gmail.com>
2024-03-25 21:14:34 -0700

    fix

--- vars/pythonBuild.groovy
+++ vars/pythonBuild.groovy
@@ -1,14 +1,15 @@
-def call() {
+// vars/testFunction.groovy
+def call(String message) {
     pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            echo "TESTTEST"
+        agent any
+        stages {
+            stage('Test Stage') {
+                steps {
+                    echo "Hello from the shared library!"
+                    echo "Message: ${message}"
+                }
+            }
         }
     }
-
-
 }
-
 }
