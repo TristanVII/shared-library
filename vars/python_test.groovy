e1c9d60347d388d63ac22c5edceabaa8c8759d7b
Tristan Davis <Tristandavis888@gmail.com>
2024-03-26 11:23:06 -0700

    add Package stage

--- vars/python_test.groovy
+++ vars/python_test.groovy
@@ -1,4 +1,5 @@
-def call(dir) {
+// Directory is path to service, also docker repo name
+def call(dir imageName, build) {
     pipeline {
     agent any
     environment {
@@ -26,12 +27,19 @@ def call(dir) {
             }
         }
         stage('Package') {
+            when {
+                expression { ${build} == true }
+            }
             steps {
-                script {
-                    echo 'Package'
+                withCredentials([string(credentialsId: 'Dockerhub', variable: 'TOKEN')]) {
+                    sh "cd ./${dir}"
+                    sh "docker login -u 'tristan007' -p '$TOKEN' docker.io"
+                    sh "docker build -t ${dir}:latest --tag tristan007/${dir}:${imageName} ."
+                    sh "docker push tristan007/${dir}:${imageName}"
                 }
             }
         }
+
         stage('Deploy') {
             steps {
                 script {
