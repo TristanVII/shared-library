49e4a3dcc23c009b456ef897050d0b04cab0e350
Tristan Davis <Tristandavis888@gmail.com>
2024-03-26 12:14:54 -0700

    asdl

--- vars/python_test.groovy
+++ vars/python_test.groovy
@@ -30,13 +30,11 @@ def call(dir, imageName, build) {
         steps {
             withCredentials([string(credentialsId: 'Dockerhub', variable: 'TOKEN')]) {
                 script {
-                    dir("${dir}") {
                         sh "ls"
                         sh "docker login -u 'tristan007' -p '$TOKEN' docker.io"
                         sh "docker build -t ${dir}:latest --tag tristan007/${dir}:${imageName} ."
                         sh "docker push tristan007/${dir}:${imageName}"
                     }
-                }
             }
         }
     }
